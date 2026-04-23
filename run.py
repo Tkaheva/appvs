fcfd0b910f89836cbfb2f55dca52f6fe4e66a593
Ткачёва Анастасия Сергеевна <tkachevaanastasia1@gmail.com>
2026-04-23 22:57:08 +0300

    h

--- run.py
+++ run.py
@@ -1,11 +1,3 @@
-﻿import sys
-import os
-
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+﻿if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))  # Измените 7000 на 8080
+    app.run(host='0.0.0.0', port=port, debug=True)
